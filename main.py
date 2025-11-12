e1b5232fda47251a8d0fe221aa6ad8ac1388b4a2
esobirjon2197 <esobirjon2197@gmail.com>
2025-11-12 17:56:43 +0500

    Add string manipulation examples in main.py
    This script demonstrates various string manipulations including input handling, case conversion, and whitespace trimming.

---
+++ main.py
@@ -0,0 +1,41 @@
+# 1-m
+i = input("Ism kirit: ")
+print(i)
+print(i.upper())
+
+
+# 2-m
+matn = "PYTHON DASTURLASH TILI"
+print(matn)
+print(matn.lower())
+
+
+# 3-m
+i = input("Parol kirit: ")
+if i == "Admin":
+    print(f"{i}-xafsiz parol emas.")
+
+
+# 4-m
+n = input("Matn kirit: ")
+print(n)
+print(n.strip())
+
+
+# 5-m
+m = input("Email kirit: ")
+print(m)
+print(m.strip())
+
+
+# 6-m
+matn = "     Salom dunyo"
+print(matn)
+print(matn.lstrip())
+
+
+# 7-m
+matn = "Salom dunyo           "
+print(matn)
+print(matn.rstrip())
+
